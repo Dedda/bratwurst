de820a684da1dfcd4e9a8f20223f9c4d681d3c28
Stephan Goeppentin <dedda102@gmail.com>
2015-09-28 13:47:01 +0200

    added some patterns for easy validating

---
+++ src/main/java/org/dedda/bratwurst/parse/Patterns.java
@@ -0,0 +1,29 @@
+package org.dedda.bratwurst.parse;
+
+/**
+ * Created by dedda on 9/28/15.
+ *
+ * @author dedda
+ */
+public class Patterns {
+
+    public static final String BEGIN = "^==>$";
+    public static final String END = "^<==$";
+
+    public static final String CLASS_BEGIN = "^#\\[$";
+    public static final String CLASS_END = "^\\]$";
+
+    public static final String FUNCTION_BEGIN = "^~\\{$";
+    public static final String FUNCTION_END = "^\\}$";
+
+    public static final String FUNCTION_PARAM_FIRST = "@ \\w+ <-- \\w+";
+    public static final String FUNCTION_PARAM_OTHERS = "& \\w+ <-- \\w+";
+
+    public static final String FUNCTION_CALL = "\\w+\\{\\w+\\}( " + FUNCTION_PARAM_FIRST + ")?( " + FUNCTION_PARAM_OTHERS + ")*";
+    public static final String CLASS_INSTANTIATION = "\\[\\w+\\]";
+    public static final String VARIABLE_DECLARATION = "^\\(\\w+\\) <-- (\\w+|" + FUNCTION_CALL + "|" + CLASS_INSTANTIATION + ")";
+
+    public static final String INCLUDE = "%\\w+[\\w\\.]+%";
+
+
+}
---
+++ src/test/java/org/dedda/bratwurst/parse/PatternsTest.java
@@ -0,0 +1,61 @@
+package org.dedda.bratwurst.parse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.dedda.bratwurst.parse.Patterns.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by dedda on 9/28/15.
+ *
+ * @author dedda
+ */
+@RunWith(Parameterized.class)
+public class PatternsTest {
+
+    @Parameters
+    public static Collection<Object[]> getParams() {
+        return Arrays.asList(new Object[][]{
+                {BEGIN, "==>", true},
+                {END, "<==", true},
+                {CLASS_BEGIN, "#[", true},
+                {CLASS_END, "]", true},
+                {FUNCTION_BEGIN, "~{", true},
+                {FUNCTION_END, "}", true},
+                {FUNCTION_CALL, "test{test}", true},
+                {FUNCTION_CALL, "test{test} @ test <-- 1", true},
+                {FUNCTION_CALL, "test{test} @ test <-- 1 & test <-- 2", true},
+                {CLASS_INSTANTIATION, "[test]", true},
+                {VARIABLE_DECLARATION, "(test) <-- 123", true},
+                {VARIABLE_DECLARATION, "(test) <-- test{test} @ test <-- 2 & test <-- 3", true},
+                {VARIABLE_DECLARATION, "(test) <-- [test]", true},
+                {INCLUDE, "%test.bw%", true},
+        });
+    }
+
+    @Parameter(0)
+    public String pattern;
+    @Parameter(1)
+    public String text;
+    @Parameter(2)
+    public boolean matches;
+
+    @Test
+    public void testPattern() {
+        if (matches) {
+//            System.out.println("'" + text + "' should match pattern '" + pattern + "'");
+            assertTrue(text.matches(pattern));
+        } else {
+//            System.out.println("'" + text + "' should not match pattern '" + pattern + "'");
+            assertFalse(text.matches(pattern));
+        }
+    }
+
+}
