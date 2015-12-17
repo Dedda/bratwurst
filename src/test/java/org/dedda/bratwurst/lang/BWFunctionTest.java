b96f12d747642a91ac24f249d43baef0675c9c02
Stephan Goeppentin <dedda102@gmail.com>
2015-12-17 16:52:33 +0100

    extended function test

--- src/test/java/org/dedda/bratwurst/lang/BWFunctionTest.java
+++ src/test/java/org/dedda/bratwurst/lang/BWFunctionTest.java
@@ -23,10 +23,15 @@ import static org.junit.Assert.*;
 public class BWFunctionTest extends BratwurtstTestcase {
 
     private BWFunction function;
+    private static boolean[] executed;
 
     @Before
     public void setUp() throws Exception {
         function = new BWFunction("testFunction", instructions);
+        executed = new boolean[instructions.length];
+        for (int i = 0; i < executed.length; i++) {
+            executed[i] = false;
+        }
     }
 
     @Parameter(0)
@@ -44,14 +49,30 @@ public class BWFunctionTest extends BratwurtstTestcase {
                 {new BWInstruction[] {
                         new BWInstruction(0) {
                             @Override
-                            public void run(Scope scope) {}
+                            public void run(Scope scope) {
+                                executed[0] = true;
+                            }
+                        },
+                        new BWInstruction(0) {
+                            @Override
+                            public void run(Scope scope) {
+                                executed[1] = true;
+                            }
                         }
                 }, new BWInteger(0), new BWVariable[0]},
                 {new BWInstruction[] {
                         new BWInstruction(0) {
                             @Override
-                            public void run(Scope scope) {}
-                        }, new Return(0, new BWInteger(1))
+                            public void run(Scope scope) {
+                                executed[0] = true;
+                            }
+                        }, new Return(0, new BWInteger(1){
+                    @Override
+                    public void run(Scope scope) {
+                        super.run(scope);
+                        executed[1] = true;
+                    }
+                })
                 }, new BWInteger(1), new BWVariable[0]}
         });
     }
@@ -62,5 +83,9 @@ public class BWFunctionTest extends BratwurtstTestcase {
         function.run(new Scope(new Program()));
         assertEquals(expectedValue.getValueType(), function.getValueType());
         assertEquals(expectedValue.getIntValue(), function.getIntValue());
+        for (int i = 0; i < executed.length; i++) {
+            boolean isExecuted = executed[i];
+            assertTrue("instruction " + i + " wan't executed!", isExecuted);
+        }
     }
 }
