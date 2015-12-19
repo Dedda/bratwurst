d9ef4eef4fd8143916f541fe87de29538ab2b225
Stephan Göppentin <dedda102@gmail.com>
2015-12-19 16:38:59 +0100

    adjusted start script

--- run-testprogram.sh
+++ run-testprogram.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ ! -f ./target/base-2.0-SNAPSHOT-jar-with-dependencies.jar ]; then
+if [ ! -f ./target/bratSe-2.0-SNAPSHOT-jar-with-dependencies.jar ]; then
     ./jar.sh
 fi
-java -jar ./target/base-2.0-SNAPSHOT-jar-with-dependencies.jar ./src/test/testprogram.bw
+java -jar ./target/bratSe-2.0-SNAPSHOT-jar-with-dependencies.jar ./src/test/testprogram.bw
