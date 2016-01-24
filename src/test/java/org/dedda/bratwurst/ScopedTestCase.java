46d3c9e7d6c1a30c230aea2902911232d8ed0204
Stephan Goeppentin <dedda102@gmail.com>
2016-01-24 19:18:37 +0100

    added tests

---
+++ src/test/java/org/dedda/bratwurst/ScopedTestCase.java
@@ -0,0 +1,19 @@
+package org.dedda.bratwurst;
+
+import org.dedda.bratwurst.lang.Program;
+import org.dedda.bratwurst.lang.scope.Scope;
+
+/**
+ * Created by dedda on 1/24/16.
+ *
+ * @author dedda
+ */
+public class ScopedTestCase extends BratwurtstTestcase {
+
+    protected Scope createEmptyScope() {
+        Program program = new Program();
+        Scope scope = new Scope(program);
+        return scope;
+    }
+
+}
---
+++ src/test/java/org/dedda/bratwurst/SuiteTest.java
@@ -0,0 +1,23 @@
+package org.dedda.bratwurst;
+
+import junit.framework.Assert;
+import org.dedda.bratwurst.test.TestSuite;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by dedda on 1/24/16.
+ *
+ * @author dedda
+ */
+public class SuiteTest {
+
+    @Test
+    public void testSuite() {
+        TestSuite testSuite = new TestSuite("src/test/testSuite.bw");
+        boolean success = testSuite.run();
+        assertTrue("Test suite failed!", success);
+    }
+
+}
